4b975c0f6112cf5299b9f606510a8852015bf196
N <noel.mcloughlin@gmail.com>
2018-12-24 03:50:37 +0000

    Basic WIA integration

---
+++ wia/sensehat_wia.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+### run as 'sudo -E ./sensehat_wia.py
+
+import os
+from time import sleep
+from wia import Wia
+from sense_hat import SenseHat
+sense = SenseHat()
+
+wia = Wia()
+wia.access_token = os.environ['WIA_ACCESS_TOKEN_SENSEHAT']
+
+while True:
+    temperature=round(sense.get_temperature(),2)
+    humidity=round(sense.get_humidity(), 2)
+    pressure=round(sense.get_pressure(), 2)
+    wia.Event.publish(name="temperature", data=temperature)
+    wia.Event.publish(name="humidity", data=humidity)
+    wia.Event.publish(name="pressure", data=pressure)
+    sleep(30)
